b1236342531a358693ff024fd5c05ec1c9e3a6a5
ThiagoR19 <riffothiago19@gmail.com>
2025-04-03 16:32:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const buttons = document.getElementById('buttons')
+const image = document.getElementById('image')
+const h1 = document.getElementById('h1')
+const h2 = document.getElementById('h2')
+const image_prev = document.getElementById('img_prev')
+
+let points = 0
+let cases = ['Aa','Ab','E','F','Ga','Gb','Gc','Gd','H','Ja','Jb','Na','Nb','Ra','Rb','T','Ua','Ub','V','Y','Z']
+let fragment = document.createDocumentFragment()
+let imageSelected = document.createElement('IMG')
+let [randomCase, randomImage] = setRandomValues()
+
+function setRandomValues () {
+  let randomCase = Math.floor(Math.random() * cases.length)
+  let randomImage = Math.floor(Math.random()*4 + 1)
+  return [randomCase, randomImage]
+}
+
+for (let i = 0; i < cases.length; i++) {
+  let button = document.createElement('BUTTON')
+  button.innerHTML = cases[i]
+  button.classList.add('buttons')
+  button.setAttribute('id',`${cases[i].toLowerCase()}`)
+
+  button.addEventListener('click',()=>{
+    if (cases[i].toLowerCase() == cases[randomCase].toLowerCase()) {
+      points++
+      h1.innerHTML = points
+      h2.innerHTML = `El anterior era: ${cases[randomCase]}`  
+      image_prev.setAttribute('src', `Cases/${cases[randomCase].toLowerCase()}/${cases[randomCase].toLowerCase()}_${randomImage}.jpeg`);
+    } else {
+      h2.innerHTML = `El anterior era: ${cases[randomCase]}` 
+      image_prev.setAttribute('src', `Cases/${cases[randomCase].toLowerCase()}/${cases[randomCase].toLowerCase()}_${randomImage}.jpeg`);
+    }
+
+    [randomCase, randomImage] = setRandomValues()
+    imageSelected.setAttribute('src', `Cases/${cases[randomCase].toLowerCase()}/${cases[randomCase].toLowerCase()}_${randomImage}.jpeg`);
+      
+  })
+  fragment.appendChild(button)
+}
+
+buttons.appendChild(fragment)
+
+imageSelected.setAttribute('src', `Cases/${cases[randomCase].toLowerCase()}/${cases[randomCase].toLowerCase()}_${randomImage}.jpeg`);
+image.appendChild(imageSelected)
